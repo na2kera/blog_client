3d24df7f01ed29fb41302e47b7ce8cd7fea71433
na2kera <nat3mitaro@gmail.com>
2024-04-20 16:07:28 +0900

    削除機能

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { Post } from "@/types";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +26,18 @@ export async function getStaticProps() {
 }
 
 export default function Home({ posts }: Props) {
+  const router = useRouter();
+
+  const handleDelete = async (postId: string) => {
+    try {
+      await axios.delete(`http://localhost:3001/api/v1/posts/${postId}`);
+
+      router.reload();
+    } catch (err) {
+      alert("削除に失敗しました。");
+    }
+  };
+
   return (
     <div className={styles.homeContainer}>
       <h2>Rails & Next.js Blog</h2>
@@ -36,10 +50,22 @@ export default function Home({ posts }: Props) {
             <Link href={`posts/${post.id}`} className={styles.postCardBox}>
               <h2>{post.title}</h2>
               <p>{post.content}</p>
-              <Link href={`/edit-post/${post.id}`}>
-                <button className={styles.editButton}>Edit</button>
-              </Link>
-              <button className={styles.deleteButton}>Delete</button>
+              <button
+                className={styles.editButton}
+                onClick={() => {
+                  router.push(`/edit-post/${post.id}`);
+                }}
+              >
+                Edit
+              </button>
+              <button
+                className={styles.deleteButton}
+                onClick={() => {
+                  handleDelete(post.id);
+                }}
+              >
+                Delete
+              </button>
             </Link>
           </div>
         ))}
